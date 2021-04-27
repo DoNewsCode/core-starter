d922f49d1e128795a0b9a52a174082466edcdc69
nfangxu <nfangxu@gmail.com>
2021-04-27 17:29:55 +0800

    fix merge

--- app/module.go
+++ app/module.go
@@ -2,27 +2,46 @@ package app
 
 import (
 	"github.com/DoNewsCode/core-starter/app/commands"
+	pb "github.com/DoNewsCode/core-starter/app/proto"
+	"github.com/DoNewsCode/core-starter/app/service"
 	"github.com/DoNewsCode/core/contract"
 	"github.com/DoNewsCode/core/di"
+	"github.com/go-kratos/kratos/v2/middleware"
+	"github.com/go-kratos/kratos/v2/middleware/logging"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/transport/http"
+	"github.com/gorilla/mux"
 	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
 )
 
-// New Create a new module
-func New(config contract.ConfigAccessor) Module {
-	return Module{config: config}
+func New(config contract.ConfigAccessor, app pb.AppServer) Module {
+	return Module{config: config, app: app}
 }
 
-// Providers Register the dependencies of this module
 func Providers() di.Deps {
-	return di.Deps{}
+	return di.Deps{
+		service.NewAppService,
+	}
 }
 
-// Module is a main file
 type Module struct {
 	config contract.ConfigAccessor
+	app    pb.AppServer
 }
 
-// ProvideCommand Marks that this module provides some commands
+func (m Module) ProvideGRPC(server *grpc.Server) {
+	// @TODO apply middlewares
+	pb.RegisterAppServer(server, m.app)
+}
+
+func (m Module) ProvideHTTP(router *mux.Router) {
+	router.PathPrefix("/").Handler(pb.NewAppHandler(m.app, http.Middleware(middleware.Chain(
+		recovery.Recovery(),
+		logging.Server(),
+	))))
+}
+
 func (m Module) ProvideCommand(command *cobra.Command) {
 	command.AddCommand(
 		commands.NewExampleCommand(m.config),
--- go.mod
+++ go.mod
@@ -4,6 +4,10 @@ go 1.16
 
 require (
 	github.com/DoNewsCode/core v0.4.2
+	github.com/go-kratos/kratos/v2 v2.0.0-beta3 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/gorilla/mux v1.8.0
 	github.com/spf13/cobra v1.1.3
+	google.golang.org/grpc v1.37.0 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
 )
